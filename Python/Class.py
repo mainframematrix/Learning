a93cee3d9b55dd23013cf32f6683b6ef94c7681a
Shashank Sahay <sahayshashank28@gmail.com>
2025-01-12 17:44:03 +0530

    LEARNING v1.3.2
    This commit has the following additions:
    1. Simple Inheritance of class in Python
    2. Variable Name change for hierarchical interitance example

--- Python/Class.py
+++ Python/Class.py
@@ -22,6 +22,34 @@ class Dog:
         """
         print("I have barked "+str(self.count)+" times")
 
+class Animal_Simple():
+    def __init__(self):
+        self.count = 0
+
+    def remember(self):
+        """
+        Remembers the number of times it this its task!
+        """
+        print("I have "+self.task+"ed "+str(self.count)+" times")
+
+class Dog_Simple(Animal_Simple):
+    """
+    A class that barks!
+    """
+
+    def __init__(self):
+        """
+        Initializes with parent class (i.e. Animal) and has a task to bark.
+        """
+        super().__init__()
+        self.task = "bark"
+
+    def bark(self):
+        """
+        Prints "Woof!!"
+        """
+        self.count += 1
+        print("Woof!!")
 
 class Animal_Hierarchical:
     def __init__(self):
@@ -83,13 +111,20 @@ if __name__ == "__main__":
     dog.bark()
     dog.remember()
     print("\n\n")
+    # Demonstate class call with simple inheritance
+    print("Demonstate class call with simple inheritance")
+    dog_simple = Dog_Simple()
+    dog_simple.bark()
+    dog_simple.bark()
+    dog_simple.remember()
+    print("\n\n")
     # Demonstrate class call with Hierarchical Inheritance.
     print("Demonstrate class call with Hierarchical Inheritance.")
-    dog = Dog_Hierarchical()
-    dog.bark()
-    dog.bark()
-    dog.remember()
-    cat = Cat_Hierarchical()
-    cat.meow()
-    cat.remember()
+    dog_hierarchical = Dog_Hierarchical()
+    dog_hierarchical.bark()
+    dog_hierarchical.bark()
+    dog_hierarchical.remember()
+    cat_hierarchical = Cat_Hierarchical()
+    cat_hierarchical.meow()
+    cat_hierarchical.remember()
     print("\n\n")
