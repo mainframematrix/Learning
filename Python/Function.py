983a786d53c53773379d8d9c8f0a2695fd1c0920
Shashank Sahay <sahayshashank28@gmail.com>
2025-01-13 23:11:10 +0530

    LEARNING v1.2.1
    This commit has the following additions:
    1. Returning function in Python

--- Python/Function.py
+++ Python/Function.py
@@ -13,7 +13,22 @@ def hello_with_parameters(name: str):
 
     print("Hello, "+name+"!")
 
+def return_hello(name: str):
+    """
+    Function that returns the string concatenated with the name parameter.
+    """
+    return "Hello, "+name+"!"
 
 if __name__ == "__main__":
+    # Calling hello() function
+    print("Calling hello() function...")
     hello()
+    print("\n\n")
+    # Calling hello_with_parameters(name: str) function
+    print("Calling hello_with_parameters(name: str) function...")
     hello_with_parameters(name="Github")
+    print("\n\n")
+    # Calling return_hello(name: str) function
+    print("Calling return_hello(name: str) function...")
+    print(return_hello("Github"))
+    print("\n\n")
