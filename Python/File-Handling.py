8f5ded434eb691371ca093fe2ea884ccd41b2d62
Shashank Sahay <sahayshashank28@gmail.com>
2025-02-01 21:49:13 +0530

    LEARNING v1.4.2
    This commit has the following additions:
    1. Writing to a file in Python

--- Python/File-Handling.py
+++ Python/File-Handling.py
@@ -4,7 +4,17 @@ def read(file_location: str):
     print(content)
     file.close()
 
+def write(file_location: str):
+    file= open(file_location,"w")
+    file.write("Hi Github, I am writing to this file.")
+    print("Verification of writing the file:\n")
+    file.close()
+    read(file_location)
+
 if __name__ == "__main__":
     print("Reading from a file...\n")
     read(file_location="Hello.txt")
+    print("\n\n")
+    print("Writing to a file...\n")
+    write(file_location="Hello_write.txt")
     print("\n\n")
