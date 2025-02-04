551db13905ad724a30ae0b724c9936b4ef9ae10d
Shashank Sahay <sahayshashank28@gmail.com>
2025-02-04 23:10:36 +0530

    LEARNING v1.4.4
    This commit has the following additions:
    1. Appending to a file in Python
    2. Pass content to write function to write to a file in Python

--- Python/File-Handling.py
+++ Python/File-Handling.py
@@ -7,13 +7,22 @@ def read(file_location: str):
     print(content)
     file.close()
 
-def write(file_location: str):
+def write(file_location: str, content: str):
     """
     Writes to a file
     """
     file= open(file_location,"w")
-    file.write("Hi Github, I am writing to this file.")
+    file.write(content)
     print("Verification of writing the file:\n")
+    file.close()
+    read(file_location)
+def append(file_location: str, content: str):
+    """
+    Appends to a file
+    """
+    file=open(file_location,"a")
+    file.write(content)
+    print("Verification of appending the file:\n")
     file.close()
     read(file_location)
 
@@ -22,5 +31,8 @@ if __name__ == "__main__":
     read(file_location="Hello.txt")
     print("\n\n")
     print("Writing to a file...\n")
-    write(file_location="Hello_write.txt")
+    write(file_location="Hello_write.txt",content="Hi Github, I am writing to this file.\n")
+    print("\n\n")
+    print("Appending to a file...\n")
+    append(file_location="Hello_write.txt",content="Hi Github, I am appending to this file.")
     print("\n\n")
